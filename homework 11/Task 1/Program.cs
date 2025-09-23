53405aa5ee4c1dd21871132dcd75f43326068839
lortkipa <nikusha191208@gmail.com>
2025-09-23 12:15:07 +0400

    first homework after summer :)

---
+++ homework 11/Task 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create persons
+            Person p1 = new Person("Nikoloz", "Lortkipanidze", 16);
+            Person p2 = new Person("Lionel", "Messi");
+
+            // print person infos
+            p1.Print();
+            p2.Print();
+        }
+    }
+
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        // age = 0 საშუალებას გვაძლევს ეს field შეუყვანელი დავტოვოთ
+        public Person(string firstName, string lastName, int age = 0)
+        {
+            // store person info
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+        public void Print()
+        {
+            // print person fields
+            Console.WriteLine($"FirstName: {_firstName}, LastName: {_lastName}, Age: {_age}");
+        }
+    }
+}
---
+++ homework 11/Task 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create books
+            Book b1 = new Book();
+            Book b2 = new Book("Cosmos", "Carl Sagan", 9.99, 1980);
+
+            // print book infos
+            b1.Print();
+            b2.Print();
+        }
+    }
+
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private double _price;
+        private int _year;
+
+        public Book()
+        {
+            // set default values to fields
+            _title = "Unknow";
+            _author = "Unknow";
+            _price = 0;
+            _year = 2025; // default year is current year
+        }
+
+        public Book(string title, string author, double price, int year)
+        {
+            // asign provided values to fields
+            _title = title;
+            _author = author;
+            _price = price;
+            
+            // make sure publication year is not in future :d
+            if (year > 2025)
+                _year = 2025;
+            else
+                _year = year;
+        }
+
+        public void Print()
+        {
+            // print fields
+            Console.WriteLine($"Title: {_title}, Author: {_author}, Price: {_price}, Year: {_year}");
+        }
+    }
+}
